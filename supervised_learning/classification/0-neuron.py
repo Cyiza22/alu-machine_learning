c7c04035bc1d0aca924a57157037f8c72d63c797
Cyiza22 <h.cyiza@alustudent.com>
2025-01-27 22:51:31 +0200

    changes

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -26,4 +26,5 @@ class Neuron:
             raise ValueError("nx must be a positive integer")
         self.W = np.random.randn(1, nx)
         self.b = 0
-        self.A = 0
+        self.A = 0 
+        
--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -1,35 +1,54 @@
 #!/usr/bin/env python3
+"""
+The definition of a single neuron performing binary classification
 
+A bit more complex than the previous version
 """
-Neuron class
-This module contains the Neuron class used for machine learning.
-"""
+
 
 import numpy as np
 
+
 class Neuron:
     """
-    Neuron class
-    Represents a single neuron performing binary classification.
-    """
+    This class mimics the behavior of a single neuron in a neural network
 
+    nx: The number of input features to the neuron
+    """
 
     def __init__(self, nx):
         """
-        Initialize a Neuron instance.
+        Initializes a neuron
 
-        Parameters:
-        nx (int): The number of input features to the neuron.
-
-        Raises:
-        TypeError: If nx is not an integer.
-        ValueError: If nx is less than 1.
+        nx is a positive integer that represents the number
+        of input features to the neuron
         """
-        if not isinstance(nx, int):
-            raise TypeError('nx must be an integer')
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError('nx must be a positive integer')
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
 
-        self.W = np.random.randn(1, nx)
-        self.b = 0
-        self.A = 0
+    @property
+    def W(self):
+        """
+        Getter for private class attribute __W
+        """
+        return (self.__W)
+
+    @property
+    def b(self):
+        """
+        Getter for private class attribute __b
+        """
+        return (self.__b)
+
+    @property
+    def A(self):
+        """
+        Getter for private class attribute __A
+        """
+        return (self.__A)
+    
